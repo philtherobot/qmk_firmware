730eb0e304b471dfdbc1b4f4cb063dec0b87f98d
Philippe Payant <philtherobot@gmail.com>
2024-10-21 21:31:15 -0400

    Got all four layers. Missing the special Shift-Extend layer lock and macro expansion.

---
+++ keyboards/handlebar/config.h
@@ -0,0 +1,2 @@
+// See `docs/feature/unicode.md`.
+#define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE, UNICODE_MODE_LINUX
--- keyboards/handlebar/keymaps/default/keymap.c
+++ keyboards/handlebar/keymaps/default/keymap.c
@@ -22,15 +22,184 @@
    { bot_right_5, bot_right_4, bot_right_3, bot_right_2, bot_right_1 }, \
    { hom_right_6, thu_right_4, thu_right_3, thu_right_2, thu_right_1 }
 
+enum layers{
+  BASE,
+  EXTEND,
+  SYMBOLS1,
+  FN,
+};
+
+
+enum unicode_names {
+    agrav,    Agrav,
+    acirc,    Acirc,
+    adiae,    Adiae,
+
+    egrav,    Egrav,
+    eacut,    Eacut,
+    ecirc,    Ecirc,
+    ediae,    Ediae,
+
+    icirc,    Icirc,
+    idiae,    Idiae,
+
+    ocirc,    Ocirc,
+
+    ugrav,    Ugrav,
+    ucirc,    Ucirc,
+    udiae,    Udiae,
+
+    ccedi,    Ccedi,
+
+    LAngQuot,
+    RAngQuot
+};
+
+const uint32_t unicode_map[] PROGMEM = {
+    [agrav]     = 0x00E0, // à
+    [Agrav]     = 0x00C0, // À
+
+    [acirc]     = 0x00E2, // â
+    [Acirc]     = 0x00C2, // Â
+
+    [adiae]     = 0x00E4, // ä
+    [Adiae]     = 0x00C4, // Ä
+
+    [egrav]     = 0x00E8, // è
+    [Egrav]     = 0x00C8, // È
+
+    [eacut]     = 0x00E9, // é
+    [Eacut]     = 0x00C9, // É
+
+    [ecirc]     = 0x00EA, // ê
+    [Ecirc]     = 0x00CA, // Ê
+
+    [ediae]     = 0x00EB, // ë
+    [Ediae]     = 0x00CB, // Ë
+
+    [icirc]     = 0x00EE, // î
+    [Icirc]     = 0x00CE, // Î
+
+    [idiae]     = 0x00EF, // ï
+    [Idiae]     = 0x00CF, // Ï
+
+    [ocirc]     = 0x00F4, // ô
+    [Ocirc]     = 0x00D4, // Ô
+
+    [ugrav]     = 0x00F9, // ù
+    [Ugrav]     = 0x00D9, // Ù
+
+    [ucirc]     = 0x00FB, // û
+    [Ucirc]     = 0x00DB, // Û
+
+    [udiae]     = 0x00FC, // ü
+    [Udiae]     = 0x00DC, // Ü
+
+    [ccedi]     = 0x00E7, // ç
+    [Ccedi]     = 0x00C7, // Ç
+
+    [LAngQuot]  = 0x00AB, // «
+    [RAngQuot]  = 0x00BB, // »
+};
+
+enum custom_keycodes {
+    CUSTOM_KC_EMAIL = SAFE_RANGE,
+    CUSTOM_KC_EXTEND
+};
+
+// bool locked_into_extend = false;
+// bool extend_is_pressed = false;
+
+// void toggle_extend_layer(void) {
+//     locked_into_extend = !locked_into_extend;
+
+//     if(locked_into_extend)
+//         layer_on(EXTEND);
+//     else
+//         layer_off(EXTEND);
+// }
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     if (record->event.pressed) {
+//         switch (keycode) {
+//             case CUSTOM_KC_EMAIL:
+//                  SEND_STRING("philtherobot@gmail.com");
+//                  return false;
+//             case CUSTOM_KC_EXTEND:
+//                 extend_is_pressed = true;
+//                 bool const isRightShift = get_mods() & MOD_BIT(KC_RIGHT_SHIFT);
+//                 if(isRightShift) {
+//                     toggle_extend_layer();
+//                 }
+//                 else {
+//                     layer_on(EXTEND);
+//                 }
+//                 return false;
+
+//             case KC_RIGHT_SHIFT:
+//                 if(extend_is_pressed) {
+//                     toggle_extend_layer();
+//                     return false;
+//                 }
+//                 return true;
+//         }
+//     }
+//     else {  // key is released
+
+//         switch (keycode) {
+//             case CUSTOM_KC_EXTEND:
+//                 extend_is_pressed = false;
+//                 if(!locked_into_extend) layer_off(EXTEND);
+//                 return false;
+//         }
+//     }
+
+//     return true;
+// }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = 
+    [BASE] = 
     { VISUAL_TO_MATRIX( 
-                        KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,
-      KC_1,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_0,
-      KC_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_BSLS,
-               KC_X,    KC_C,    KC_D,    KC_V,    KC_Z,        KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,
-                        KC_LCTL, KC_LSFT, KC_SPC,  KC_LALT,     KC_X,    KC_ENT,  KC_RSFT, KC_X
+                            KC_2,    KC_3,    KC_4,    KC_5,        KC_6,           KC_7,    KC_8,     KC_9,
+      KC_1,        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,           KC_L,    KC_U,     KC_Y,    KC_SCLN, KC_0,
+      MO(EXTEND),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,           KC_N,    KC_E,     KC_I,    KC_O,    KC_BSLS,
+                   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_SLSH,        KC_K,    KC_H,     KC_COMM, KC_DOT,
+                            KC_LCTL, KC_LSFT, KC_SPC,  KC_LALT,     OSL(SYMBOLS1),  KC_ENT,  KC_RSFT,  MO(FN)
+    )
+    },
+
+
+    [EXTEND] = 
+    { VISUAL_TO_MATRIX( 
+      //-----------//-----------//-----------//-----------//-----------//-----------     //-----------//-----------//-----------//-----------//-----------//-----------
+                                _______,     _______,     _______,     _______,          _______,     _______,     _______,     _______,
+      _______,     _______,     KC_ESC,      KC_INS,      KC_CAPS,     KC_MS_UP,         KC_PGUP,     KC_HOME,     KC_UP,       KC_END,      KC_DEL,      _______,
+      _______,     _______,     KC_LALT,     KC_LSFT,     KC_LCTL,     KC_MS_DOWN,       KC_PGDN,     KC_LEFT,     KC_DOWN,     KC_RIGHT,    KC_BSPC,     _______,
+                   _______,     LCTL(KC_X),  LCTL(KC_C),  LCTL(KC_D),  LCTL(KC_V),       KC_MS_BTN1,  KC_MS_BTN3,  KC_MS_BTN2,  KC_MS_LEFT,  KC_MS_RIGHT,
+                                _______,     LCTL(KC_Z),  _______,     _______,          KC_TAB,      _______,     _______,     _______
+    )
+    },
+
+
+    [SYMBOLS1] = 
+    { VISUAL_TO_MATRIX( 
+      //-----------//-------------//-------------//-----------------//-----------//-----------     //----------------//-----------------//------------------//-----------------//-----------------//-----------
+                                  _______,       _______,           _______,     _______,          _______,           _______,           _______,           KC_MINS,
+      KC_GRV,      UM(LAngQuot),  UM(RAngQuot),  S(KC_LBRC),        S(KC_RBRC),  _______,          _______,           UP(agrav, Agrav),  UP(ocirc, Ocirc),  UP(acirc, Acirc),  UP(icirc, Icirc),  KC_QUOT,
+      _______,     KC_LBRC,       KC_RBRC,       S(KC_9),           S(KC_0),     _______,          UP(idiae, Idiae),  UP(eacut, Eacut),  UP(egrav, Egrav),  UP(ecirc, Ecirc),  UP(ediae, Ediae),  _______,
+                   _______,       _______,       UP(ccedi, Ccedi),  KC_EQL,      S(KC_EQL),        _______,           _______,           UP(ucirc, Ucirc),  UP(ugrav, Ugrav),  UP(udiae, Udiae),
+                                  _______,       _______,           _______,     _______,          _______,           _______,           _______,           _______
+    )
+    },
+
+    [FN] = 
+    { VISUAL_TO_MATRIX( 
+      //-----------//----------------//-----------//-----------//-----------//-----------     //-----------//-----------//-----------//-----------//-----------//-----------
+                                     KC_F2,       KC_F3,       KC_F4,       KC_F5,            KC_F6,       KC_F7,       KC_F8,       KC_F9,
+      KC_F1,       _______,          _______,     KC_VOLD,     KC_MUTE,     KC_VOLU,          KC_F11,      KC_F12,      _______,     KC_PSCR,     KC_PAUSE,    KC_F10,
+      _______,     _______,          _______,     KC_MPRV,     KC_MPLY,     KC_MNXT,          _______,     _______,     _______,     _______,     _______,     _______,
+                   CUSTOM_KC_EMAIL,  _______,     _______,     _______,     _______,          _______,     _______,     _______,     _______,     _______,
+                                     _______,     _______,     KC_LGUI,     _______,          _______,     _______,     KC_RALT,     _______
     )
     }
 };
